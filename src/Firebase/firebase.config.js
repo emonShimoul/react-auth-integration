074aab6daf937f2e3eec4a69846646ca1310519d
emonShimoul <emonshimoul@gmail.com>
2022-06-10 11:31:52 +0600

    react authenticaton system in realistic application

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxJXE_B-EYmYRxAIA-iV4UlTK6f4kf978",
+    authDomain: "simple-firebase-authenti-c52ff.firebaseapp.com",
+    projectId: "simple-firebase-authenti-c52ff",
+    storageBucket: "simple-firebase-authenti-c52ff.appspot.com",
+    messagingSenderId: "521523969165",
+    appId: "1:521523969165:web:9d87efc51781296395b6c4"
+  };
+
+  export default firebaseConfig;
