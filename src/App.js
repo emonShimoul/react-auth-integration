e4556476a8e0413a9eb21944d128593ba30fde7a
emonShimoul <emonshimoul@gmail.com>
2022-06-10 19:48:23 +0600

    setup useAuth custom hook and create shipping route

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Header from './components/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './components/Shipping/Shipping';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Header></Header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/shipping" element={<Shipping />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
